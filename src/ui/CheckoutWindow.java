7ab55401104431e4d54202d1fbf02752145ae67a
Phong Lu <lutaiphong@gmail.com>
2022-06-03 15:56:20 -0500

    Update checkout window.

--- src/ui/CheckoutWindow.java
+++ src/ui/CheckoutWindow.java
@@ -1,103 +1,143 @@
 package ui;
 
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.List;
-import java.util.Vector;
-
-import javax.swing.*;
-
 import business.SystemController;
 import entities.Book;
 import entities.LibraryMember;
 import models.ComboBoxItem;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.List;
+import java.util.Vector;
+
 public class CheckoutWindow extends JFrame {
-	public static void main(String[] args) {
-		EventQueue.invokeLater(() -> new CheckoutWindow().setVisible(true));
-	}
+    JLabel lblMaxCheckoutLengthValue = new JLabel();
 
-	public CheckoutWindow() {
-		setBounds(100, 100, 400, 300);
-		initialize();
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> new CheckoutWindow().setVisible(true));
+    }
 
-	private void initialize() {
-		Container container = getContentPane();
-		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-		container.add(LoadMembers());
-		container.add(LoadBooks());
-	}
+    public CheckoutWindow() {
+        setBounds(100, 100, 400, 300);
+        initialize();
+    }
 
-	private Container LoadMembers() {
-		List<LibraryMember> members = new SystemController().allMembers();
-		Vector model = new Vector();
-		for (LibraryMember libraryMember : members) {
-			String id = libraryMember.getMemberId();
-			String name = libraryMember.getFirstName() + " " + libraryMember.getLastName();
-			model.addElement(new ComboBoxItem(id, name ));
-		}
+    private void initialize() {
+        Container container = getContentPane();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        container.add(LoadMembers());
+        container.add(LoadBooks());
 
-		JComboBox comboBox = new JComboBox(model);
-		JLabel lblMember = new JLabel("Member");
+        List<Book> books = new SystemController().allBooks();
+        if (books.size() > 0) {
+            container.add(LoadDate(books.get(0)));
+        }
 
-		Container container = new Container();
-		container.setLayout(new FlowLayout(FlowLayout.LEFT));
-		container.add(lblMember);
-		container.add(comboBox);
+        container.add(LoadCheckoutDate());
+    }
 
-		return container;
-	}
+    private Container LoadMembers() {
+        List<LibraryMember> members = new SystemController().allMembers();
+        Vector model = new Vector();
+        for (LibraryMember libraryMember : members) {
+            String id = libraryMember.getMemberId();
+            String name = libraryMember.getFirstName() + " " + libraryMember.getLastName();
+            model.addElement(new ComboBoxItem(id, name));
+        }
 
-	private Container LoadBooks() {
-		List<Book> books = new SystemController().allBooks();
-		Vector model = new Vector();
-		for (Book book : books) {
-			String id = book.getId();
-			String name = book.getTitle();
-			model.addElement(new ComboBoxItem(String.valueOf(id), name ));
-		}
+        JComboBox comboBox = new JComboBox(model);
+        JLabel lblMember = new JLabel("Member");
 
-		JComboBox comboBox = new JComboBox(model);
-		comboBox.addItemListener(e -> {
-			ComboBoxItem item = (ComboBoxItem)e.getItem();
+        Container container = new Container();
+        container.setLayout(new FlowLayout(FlowLayout.LEFT));
+        container.add(lblMember);
+        container.add(comboBox);
 
-			Book foundBook = null;
+        return container;
+    }
 
-			for (Book book : books) {
-				if (book.getId().equals(item.getId())) {
-					foundBook = book;
-					break;
-				}
-			}
+    private Container LoadBooks() {
+        List<Book> books = new SystemController().allBooks();
+        Vector model = new Vector();
+        for (Book book : books) {
+            String id = book.getId();
+            String name = book.getTitle();
+            model.addElement(new ComboBoxItem(String.valueOf(id), name));
+        }
 
-			if (foundBook != null) {
+        JComboBox comboBox = new JComboBox(model);
+        comboBox.addItemListener(e -> {
+            ComboBoxItem item = (ComboBoxItem) e.getItem();
 
-			}
-		});
+            Book foundBook = null;
 
-		JLabel lblMember = new JLabel("Books");
+            for (Book book : books) {
+                if (book.getId().equals(item.getId())) {
+                    foundBook = book;
+                    break;
+                }
+            }
 
-		Container container = new Container();
-		container.setLayout(new FlowLayout(FlowLayout.LEFT));
-		container.add(lblMember);
-		container.add(comboBox);
+            if (foundBook != null) {
+                setMaxCheckoutLengthValueLabel(foundBook.getMaxCheckoutLength());
+            }
+        });
 
-		return container;
-	}
+        JLabel lblMember = new JLabel("Books");
 
-	private Container LoadDate(Book book) {
-		JLabel lblMember = new JLabel("Max checkout date");
-		JLabel lblMaxCheckoutLength = new JLabel(String.valueOf(book.getMaxCheckoutLength()));
+        Container container = new Container();
+        container.setLayout(new FlowLayout(FlowLayout.LEFT));
+        container.add(lblMember);
+        container.add(comboBox);
 
-		Container container = new Container();
-		container.setLayout(new FlowLayout(FlowLayout.LEFT));
-		container.add(lblMember);
-		container.add(lblMaxCheckoutLength);
+        if (books.size() > 0) {
+            Book firstBook = books.get(0);
+            setMaxCheckoutLengthValueLabel(firstBook.getMaxCheckoutLength());
+        }
 
-		return container;
-	}
+        return container;
+    }
+
+    private Container LoadDate(Book book) {
+        setMaxCheckoutLengthValueLabel(book == null ? 0 : book.getMaxCheckoutLength());
+
+        JLabel lblMaxCheckoutLength = new JLabel("Max checkout length");
+
+        Container container = new Container();
+        container.setLayout(new FlowLayout(FlowLayout.LEFT));
+        container.add(lblMaxCheckoutLength);
+        container.add(this.lblMaxCheckoutLengthValue);
+
+        return container;
+    }
+
+    private Container LoadCheckoutDate() {
+        JLabel lblCheckoutLength = new JLabel("Checkout length");
+        JLabel lblCheckoutDays = new JLabel("day(s)");
+        JTextField txtCheckout = new JTextField();
+        txtCheckout.setColumns(3);
+        txtCheckout.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if (!((c >= '0') && (c <= '9') || (c == KeyEvent.VK_BACK_SPACE) || (c == KeyEvent.VK_DELETE))) {
+                    getToolkit().beep();
+                    e.consume();
+                }
+            }
+        });
+
+        Container container = new Container();
+        container.setLayout(new FlowLayout(FlowLayout.LEFT));
+        container.add(lblCheckoutLength);
+        container.add(txtCheckout);
+        container.add(lblCheckoutDays);
+
+        return container;
+    }
+
+    private void setMaxCheckoutLengthValueLabel(int length) {
+        this.lblMaxCheckoutLengthValue.setText(length + " day(s)");
+    }
 }
